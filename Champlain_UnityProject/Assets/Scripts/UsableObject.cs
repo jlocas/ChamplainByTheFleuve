1fe8b677d086a42041162b9083f90ff8c8a7905f
jlocas <jeremie.locas@hotmail.com>
2016-07-20 14:56:44 -0400

    Bunch of adds and work on the house mesh

---
+++ Champlain_UnityProject/Assets/Scripts/UsableObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UsableObject : MonoBehaviour {
+
+	[SerializeField]
+	protected bool isActive = false;
+
+	public void Trigger(){
+		if(isActive){
+			Deactivate();
+		} else {
+			Activate();
+		}
+	}
+
+	protected void Activate(){
+		Debug.Log("L'OBJET " + gameObject.name + " est activé!");
+		isActive = true;
+	}
+
+	protected void Deactivate(){
+		Debug.Log("L'OBJET " + gameObject.name + " est désactivé!");
+		isActive = false;
+	}
+}
